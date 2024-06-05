6bc67dc23bbdb690c1a059851f3f4d127a3af59d
Roger5791 <sauravantony2002@gmail.com>
2024-06-05 17:07:19 +0530

    Third-Commit

--- components/Form.jsx
+++ components/Form.jsx
@@ -4,27 +4,71 @@ import { Button } from "./ui/button"
 import { Input } from "./ui/input"
 import { Textarea } from "./ui/textarea"
 import { User, MailIcon, ArrowRightIcon, MessageSquare } from "lucide-react"
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Form = () => {
+
+  const form = useRef();
+
+  const notify = () => {
+    toast.success('Submitted', {
+      position: "bottom-right",
+      theme: "dark",
+      autoClose: 3000,
+      });
+  }
+
+  
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_tx4i3rn', 'template_end63rj', form.current, {
+        publicKey: '_-LJzA5YHNuES96kg',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  };
+
+  const handleSubmit = (event)=>{
+    event.preventDefault();
+    sendEmail(event)
+    notify()
+    event.target.reset();
+  };
+
+
   return (
-    <form className=" flex flex-col gap-y-4">
+    <form ref={form} onSubmit={handleSubmit} className=" flex flex-col gap-y-4">
         <div className="relative flex items-center mt-12">
-            <Input type='name' id='name' placeholder='Name' />
+            <Input type='name' name='name' id='name' placeholder='Name' required />
             <User className=" absolute right-6" size={20} />
         </div>
 
         <div className="relative flex items-center mt-3">
-            <Input type='email' id='email' placeholder='Email' />
+            <Input type='email' name='email' id='email' placeholder='Email' required />
             <MailIcon className=" absolute right-6" size={20} />
         </div>
 
         <div className="relative flex items-center mt-3">
-            <Textarea placeholder='Type your Message here.' />
+        
+            <Textarea type='message' name='message' id='message' placeholder='Type your Message here.' required />
             <MessageSquare className=" absolute top-4 right-6" size={20} />
         </div>
 
-        <Button className='flex items-center gap-x-2 max-w-[166px]'>Let' Talk <ArrowRightIcon /></Button>
-    </form>
+        <Button type="submit" value="Send" className='flex items-center gap-x-2 max-w-[166px]'>Let' Talk <ArrowRightIcon /></Button>
+        <ToastContainer />
+        </form>
   )
 }
 
